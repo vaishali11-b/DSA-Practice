6fb2280c3f19ca18fd533b2587a56f1e210834ff
Vaishali Baderia <baderiavishu11@gmail.com>
2024-03-07 00:49:17 +0530

    added solution part

--- leetcode/141.java
+++ leetcode/141.java
@@ -1,7 +1,32 @@
 /*
  * 141.Linked List Cycle
- *
+ * https://leetcode.com/problems/linked-list-cycle
  */
-//Problem statement
-//solution1
-//solution2
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class 141 {
+    public boolean hasCycle(ListNode head) {
+        HashSet <ListNode> map=new HashSet<ListNode>();
+
+        while(head!=null){
+            if(map.contains(head))
+            {
+                return true;
+            }
+            
+            map.add(head);
+            head=head.next;
+        }
+        return false;
+    }
+}
