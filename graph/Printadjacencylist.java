d8fd002f0dd9eaa34c70f0031d49c130f0daec1f
Vaishali <vaishali@Vaishalis-MacBook-Air.local>
2026-02-08 21:46:28 +0530

    added graph

---
+++ graph/Printadjacencylist.java
@@ -0,0 +1,31 @@
+// Given a graph with V vertices and E edges, represented as an edge list, print the adjacency list of the graph. The graph can be either directed or undirected based on the input.
+package graph;
+import java.util.*;
+public class Printadjacencylist {
+
+    public List<List<Integer>> printGraph(int V, int edges[][]) {
+        Map<Integer , List<Integer>> adj = new LinkedHashMap<>();
+        
+         // Step 1: Initialize the map for all vertices 0 to V-1
+        for (int i = 0; i < V; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+        
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+           
+            adj.get(u).add(v);
+            adj.get(v).add(u); // Remove this line if the graph is Directed
+        }
+        //return a List<List<Integer>> rather than Collection<List<Integer>>
+        return new ArrayList<>(adj.values());
+    }
+    public static void main(String[] args) {
+        Printadjacencylist obj = new Printadjacencylist();
+        int V = 5;
+        int[][] edges = {{0, 1}, {0, 2}, {1, 3}, {1, 4}};
+        List<List<Integer>> adjList = obj.printGraph(V, edges);
+        System.out.println(adjList);
+    }
+}
