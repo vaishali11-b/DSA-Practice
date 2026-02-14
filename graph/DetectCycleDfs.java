45ec2996ff19cb8e98013583066bcbd6f14972b1
Vaishali <vaishali@Vaishalis-MacBook-Air.local>
2026-02-14 18:21:21 +0530

    added file

--- graph/DetectCycleDfs.java
+++ graph/DetectCycleDfs.java
@@ -45,3 +45,54 @@ public class DetectCycleDfs {
         System.out.println(obj.isCycle(V, adj) ? "1" : "0");
     }
 }
+//Detect cycle in directed graph
+class Solution {
+    public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0 ; i < V ; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            
+            adj.get(u).add(v);
+        }
+        boolean[] visited = new boolean[V];
+        boolean[] currPath = new boolean[V];
+        for(int i=0 ; i < V ; i++){
+            if(!visited[i]){
+                if(dfs(adj , visited , currPath , i , -1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean dfs(ArrayList<ArrayList<Integer>> adj  , boolean[] visited ,
+    boolean[] currPath , int i , int parent){
+        if(currPath[i]){
+            return true;
+        }
+        if(visited[i]){
+            return false;
+        }
+        
+        visited[i] = true;
+        currPath[i] = true;
+        
+        for(int it : adj.get(i)){
+            if(!visited[it]){
+                dfs(adj , visited , currPath , it , i);
+            }
+                if(currPath[it]){
+                    //System.out.println("it"+it + "i"+i);
+                   return true; 
+                }
+        }
+        currPath[i]=false;
+        return false;
+    }
+}
